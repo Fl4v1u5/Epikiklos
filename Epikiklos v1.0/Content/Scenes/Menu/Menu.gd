47e51c416b7b48f870625d5e69d34e51b026f079
Fl4v1u5 <39311030+Fl4v1u5@users.noreply.github.com>
2018-06-08 20:09:22 +0200

    Some more files.
    Note that "redpp", "greenpp", "bluemm" stand for red plus plus (aka red plus one), green plus plus, blue plus plus, etcetera

--- Epikiklos v1.0/Content/Scenes/Menu/Menu.gd
+++ Epikiklos v1.0/Content/Scenes/Menu/Menu.gd
@@ -1,25 +1,68 @@
 extends Node
 
+
+
+func _ready():
+    set_process_input(true)
+
+
+func _input(event):
+    if event.is_action_pressed("ui_accept"): get_tree().change_scene("res://Content/Scenes/Level/Main.tscn")
+
+
 func _on_Global_pressed():
+	
 	get_tree().change_scene("res://Content/Scenes/Level/Main.tscn")
+	
+	pass
 
 func _on_LineNbOfEpicicles_text_changed(new_text):
-	global.NbOfEpcs = float(new_text)
+	
+	global.NbOfEpcs = float(new_text);
+	
+	pass
+
 
 func _on_LineNbOfSegments_text_changed(new_text):
-	global.NbOfSgmnts = int(new_text)
+	
+	global.NbOfSgmnts = int(new_text);
+	
+	pass
+
 
 func _on_LineRadiusOfCircumference_text_changed(new_text):
-	global.RadiusC = int(new_text)
+	
+	global.RadiusC = int(new_text);
+	
+	pass
+
 
 func _on_LineThiccness_text_changed(new_text):
-	global.thicc = float(new_text)
+	
+	global.thicc = float(new_text);
+	
+	pass
+
 
 func _on_LineNbOfEpicicles_text_entered(new_text):
-	global.NbOfEpcs = float(new_text)
+	
+	global.NbOfEpcs = float(new_text);
+	
+	pass # replace with function body
+
+
+
+
 
 func _on_ColorPickerButtonCirc_color_changed(color):
-	global.ColorC = color
+	
+	global.ColorC = color;
+	
+	pass # replace with function body
+
 
 func _on_ColorPickerButtonLine_color_changed(color):
-	global.ColorL = color
+	
+	global.ColorL = color;
+	
+	pass # replace with function body
--- Epikiklos v1.0/Content/Scenes/Menu/backg.gd
+++ Epikiklos v1.0/Content/Scenes/Menu/backg.gd
@@ -1,8 +1,12 @@
 extends Sprite
 
+
 var NbOfEpcs = 0.0; 
 var NbOfSgmnts = 0; 
 var RadiusC = 0; 
+var Rate_of_Change = 0.1;
 var thicc = 0; 
-var ColorC = Color(1.0, 0.0, 1.0); 
-var ColorL = Color(0.0, 1.0, 1.0);
+var ColorC = Color(1.0, 1.0, 0.0); 
+var ColorL = Color(0.6, 0.7, 0.0);
+var instruction = "redpp"
+var comp = 0
